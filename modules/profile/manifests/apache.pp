d37b8892ccd204360e120b628651399510aaf4bd
Feng Li <lifcn@yahoo.com>
2017-11-06 11:09:52 +1100

    Create 2 vhosts

--- modules/profile/manifests/apache.pp
+++ modules/profile/manifests/apache.pp
@@ -1,9 +1,31 @@
 class profile::apache {
+
   class {'::apache': }
 
-  file { '/var/www/html/index.html':
+# define first.fen9.li vhost
+  $first_html_index_content = 'wuwala from first.fen9.li...'
+
+  apache::vhost { 'first.fen9.li':
+    port    => '80',
+    docroot => '/var/www/first',
+  }
+
+  file { '/var/www/first/index.html' :
     ensure   => 'file',
-    content  => 'holy cow from first.fen9.li...',
+    content  => $first_html_index_content,
+  }
+
+# define second.fen9.li vhost
+  $second_html_index_content = 'hulala from second.fen9.li...'
+
+  apache::vhost { 'second.fen9.li':
+    port    => '80',
+    docroot => '/var/www/second',
+  }
+
+  file { '/var/www/second/index.html' :
+    ensure   => 'file',
+    content  => $second_html_index_content,
   }
 
 }
