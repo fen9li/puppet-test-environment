2edf0b16ac218bca1b8e45efba031cc50caa208f
Feng Li <fen9li@fen9.li>
2017-11-11 21:25:17 +1100

    Add metadata.json file for modules flimodule, profile, role.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,9 +2,9 @@ node 'test31.fen9.li' {
   include role::webserver
 }
 
-node 'test32.fen9.li' {
-  include role::base
-}
+#node 'test32.fen9.li' {
+#  include role::base
+#}
 
 node /esnode4[1-9].fen9.li/ {
   include role::elasticsearchnode
